50a6f48ba53dbb1f62c79be69ec37a4980321285
russellpp <russellroypalmejar@gmail.com>
2023-03-14 03:25:38 +0800

    added edit and destroyn methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,4 +23,30 @@ class ArticlesController < ApplicationController
         end
     end
 
+    def edit
+        @article = Article.find(params[:id])
+        render :edit
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:name, :body))
+            redirect_to article_url(@article)
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_url
+    end
+
+    private
+
+    def article_params
+        params.require(:article).permit(:name, :body)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
 
   get '/articles' => 'articles#index'
   get '/articles/new' => 'articles#new', as: 'new_article'
+  get '/articles/:id/edit' => 'articles#edit', as: 'edit_article'
   get '/articles/:id' => 'articles#show', as: 'article'
   post '/articles/' => 'articles#create', as: 'create_article'
+  patch '/articles/:id' => 'articles#update'
+  delete '/articles/:id' => 'articles#destroy'
 
 
   # Defines the root path route ("/")
